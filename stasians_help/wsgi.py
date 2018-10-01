267375fead1e9899faf004e30c8bd876958dcd04
Stanislav Hnatiuk <stanislavmain@gmail.com>
2018-10-01 11:50:46 +0300

    The default settings for production are set.

--- stasians_help/wsgi.py
+++ stasians_help/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stasians_help.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'stasians_help.settings.production'
+)
 
 application = get_wsgi_application()
